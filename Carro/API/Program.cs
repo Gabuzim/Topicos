cee4675e04a448ccafacffd168b6c63bb23d0ccb
GABRYEL ROCHA CARDOSO MONTEIRO <gabryel.monteiro@cs.up.edu.br>
2025-03-27 11:37:18 -0300

    Adicionado integração com BD

--- Carro/API/Program.cs
+++ Carro/API/Program.cs
@@ -1,21 +1,61 @@
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
+
 var app = builder.Build();
 
+app.MapGet("/api/carros", ([FromServices] AppDataContext ctx) => {
 
-List<Carro> carros = [
-    new Carro { Id = 1, Name = "Ferrari"},
-    new Carro { Id = 2, Name = "Fiat"}
-];
+    if(ctx.Carros.Any()){
+        return Results.Ok(ctx.Carros.ToList());
+    }
 
-app.MapGet("/", () => "Hello World!");
-//endpoint listar carros
+    return Results.NotFound();
 
-app.MapGet ("/api/carros", () => {
-    return Results.Ok(carros);
+} );
+
+    app.MapPost("/api/carros", ([FromServices] AppDataContext ctx, [FromBody] Carro carro) => {
+        ctx.Carros.Add(carro);
+        ctx.SaveChanges();
+        return Results.Created("", carro);
+        });
+
+//GET: Buscar o carro pelo ID
+app.MapGet ("/api/carros/{id}", ([FromRoute]int id, [FromServices] AppDataContext ctx) => {
+    Carro? carro = ctx.Carros.Find(id);
+    if(carro != null){
+        return Results.Ok(carro);
+    }
+    return Results.NotFound();
     });
+    // PUT: Atualiza os dados do carro pelo ID
+    app.MapPut("/api/carros/{id}", ([FromRoute]int id, [FromBody] Carro carro, [FromServices] AppDataContext ctx) => {
+        Carro? entidade = ctx.Carros.Find(id);
+        if(entidade != null){
+            entidade.Name = carro.Name;
+            ctx.Carros.Update(entidade);
+            ctx.SaveChanges();
+            return Results.Ok(entidade);
+        }
+        return Results.NotFound();
+        });
+
+
+
+        //DELETE: Remove um carro pelo ID
+     app.MapDelete("/api/carros/{id}", ([FromRoute]int id, [FromServices] AppDataContext ctx) => {
+        Carro? carro = ctx.Carros.Find(id);
+
+        if (carro == null){
+        return Results.NotFound();
+        }
+        ctx.Carros.Remove(carro);
+        ctx.SaveChanges();
+        return Results.NoContent();
+        });
 
 
 app.Run();
